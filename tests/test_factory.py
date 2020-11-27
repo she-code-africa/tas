aa2988233985770c7ff6f196cd5798ac260b4320
Josh <mail@chukwuanu.com>
2020-11-27 05:23:51 +0100

    improv: test

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,9 +4,15 @@ from srv import create_app
 def test_config():
     """Test create_app without passing test config."""
     assert not create_app().testing
-    assert create_app({"TESTING": True}).testing
+    assert create_app('testing').testing
 
 
-def test_status(client):
-    response = client.get("/status")
+def test_status(test_client):
+    response = test_client.get("/status")
     assert response.data == b"OK!"
+
+
+def test_index_page(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert response.data == b"SCA Track Assessment Server"
